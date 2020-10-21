dfa416860e3d2fae9ef26ee921b4c3c9365a71df
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-21 15:48:59 -0700

    initial commit with working interface

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+// Business Logic
+const add = function (number1, number2) {
+	return number1 + number2;
+};
+
+const subtract = function (number1, number2) {
+	return number1 - number2;
+};
+
+const multiply = function (number1, number2) {
+	return number1 * number2;
+};
+
+const divide = function (number1, number2) {
+	return number1 / number2;
+};
+
+// User Interface Logic
+$(document).ready(function () {
+	$('form#subtract').submit(function (event) {
+		event.preventDefault();
+		const number1 = parseInt($('#subtract1').val());
+		const number2 = parseInt($('#subtract2').val());
+
+		const result = subtract(number1, number2);
+		$('#output').text(result);
+	});
+});
+
+
+$(document).ready(function () {
+	$('form#multiply').submit(function (event) {
+		event.preventDefault();
+		const number1 = parseInt($('#multiply1').val());
+		const number2 = parseInt($('#multiply2').val());
+
+		const result = multiply(number1, number2);
+		$('#output').text(result);
+	});
+});
+
+$(document).ready(function () {
+	$('form#divide').submit(function (event) {
+		event.preventDefault();
+		const number1 = parseInt($('#divide1').val());
+		const number2 = parseInt($('#divide2').val());
+
+		const result = divide(number1, number2);
+		$('#output').text(result);
+	});
+});
+
+$(document).ready(function () {
+	$('form#add').submit(function (event) {
+		event.preventDefault();
+		const number1 = parseInt($('#add1').val());
+		const number2 = parseInt($('#add2').val());
+
+		const result = add(number1, number2);
+		$('#output').text(result);
+	});
+});
