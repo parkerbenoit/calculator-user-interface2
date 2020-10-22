fe586b20b22a7311fccc7fb1fbc0eb410e5f95e2
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-22 15:22:20 -0700

    unified input for all functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,47 +16,22 @@ const divide = function (number1, number2) {
 };
 
 // User Interface Logic
-$(document).ready(function () {
-	$('form#subtract').submit(function (event) {
-		event.preventDefault();
-		const number1 = parseInt($('#subtract1').val());
-		const number2 = parseInt($('#subtract2').val());
-
-		const result = subtract(number1, number2);
-		$('#output').text(result);
-	});
-});
-
-
-$(document).ready(function () {
-	$('form#multiply').submit(function (event) {
-		event.preventDefault();
-		const number1 = parseInt($('#multiply1').val());
-		const number2 = parseInt($('#multiply2').val());
-
-		const result = multiply(number1, number2);
-		$('#output').text(result);
-	});
-});
-
-$(document).ready(function () {
-	$('form#divide').submit(function (event) {
-		event.preventDefault();
-		const number1 = parseInt($('#divide1').val());
-		const number2 = parseInt($('#divide2').val());
-
-		const result = divide(number1, number2);
-		$('#output').text(result);
-	});
-});
-
-$(document).ready(function () {
-	$('form#add').submit(function (event) {
-		event.preventDefault();
-		const number1 = parseInt($('#add1').val());
-		const number2 = parseInt($('#add2').val());
-
-		const result = add(number1, number2);
-		$('#output').text(result);
-	});
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
 });
